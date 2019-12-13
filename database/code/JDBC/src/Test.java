8a91aace372628b91bf9713aa2a8cb94aac3c788
bjmashibing <alanpppbox@gmail.com>
2019-12-13 14:38:04 +0800

    update

--- database/code/JDBC/src/Test.java
+++ database/code/JDBC/src/Test.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
@@ -6,10 +7,10 @@ public class Test {
     public static void main(String[] args) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","123456");
-        PreparedStatement pstmt = conn.prepareStatement("insert into psn values(?,?)");
-        for (int i = 0; i < 20000; i++) {
+        PreparedStatement pstmt = conn.prepareStatement("insert into tblA(age,birth) values (?,?);");
+        for (int i = 200000; i < 1000000; i++) {
             pstmt.setInt(1,i);
-            pstmt.setString(2,i+"");
+            pstmt.setDate(2,new Date(new java.util.Date().getTime()));
             pstmt.addBatch();
         }
         pstmt.executeBatch();
