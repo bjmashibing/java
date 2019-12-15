edb0d03c0ee41f0d3cc89165ee31946d539a784d
bjmashibing <alanpppbox@gmail.com>
2019-12-15 17:06:44 +0800

    update

--- database/code/JDBC/src/Test.java
+++ database/code/JDBC/src/Test.java
@@ -7,13 +7,16 @@ public class Test {
     public static void main(String[] args) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","123456");
-        PreparedStatement pstmt = conn.prepareStatement("insert into tblA(age,birth) values (?,?);");
-        for (int i = 200000; i < 1000000; i++) {
-            pstmt.setInt(1,i);
-            pstmt.setDate(2,new Date(new java.util.Date().getTime()));
-            pstmt.addBatch();
+        PreparedStatement pstmt = conn.prepareStatement("insert into rental(rental_date,inventory_id,customer_id,return_date,staff_id,last_update) values (?,?,?,?,?,?);");
+        for (int i = 200000; i < 5000000; i++) {
+            pstmt.setDate(1,new Date(new java.util.Date().getTime()));
+            pstmt.setInt(2,1);
+            pstmt.setInt(3,1);
+            pstmt.setDate(4,new Date(new java.util.Date().getTime()));
+            pstmt.setInt(5,1);
+            pstmt.setDate(6,new Date(new java.util.Date().getTime()));
+            pstmt.execute();
         }
-        pstmt.executeBatch();
         conn.close();
     }
 }
