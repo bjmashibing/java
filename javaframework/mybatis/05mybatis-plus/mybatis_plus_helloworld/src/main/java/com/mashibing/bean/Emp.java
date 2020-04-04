e152cc648d27c7142ecb2277e95b05f3b0442ebe
bjmashibing <alanpppbox@gmail.com>
2020-04-04 17:10:10 +0800

    update

---
+++ javaframework/mybatis/05mybatis-plus/mybatis_plus_helloworld/src/main/java/com/mashibing/bean/Emp.java
@@ -0,0 +1,103 @@
+package com.mashibing.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import org.omg.CORBA.IDLType;
+
+import java.util.Date;
+
+@TableName("tbl_emp")
+public class Emp {
+
+    /**
+     * 在 mybatis-plus2.x版本的时候，如果设置了表自增，那么id必须制定为auto类型，否则插入不成功，3.x不存在此问题
+     */
+    @TableId(value = "empno",type = IdType.AUTO)
+    private Integer empno;
+    private String eName;
+    private String job;
+    private Integer mgr;
+    private Date hiredate;
+    private Double sal;
+    private Double comm;
+    private Integer deptno;
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+
+    public Double getComm() {
+        return comm;
+    }
+
+    public void setComm(Double comm) {
+        this.comm = comm;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empno=" + empno +
+                ", eName='" + eName + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr=" + mgr +
+                ", hiredate=" + hiredate +
+                ", sal=" + sal +
+                ", comm=" + comm +
+                ", deptno=" + deptno +
+                '}';
+    }
+}
